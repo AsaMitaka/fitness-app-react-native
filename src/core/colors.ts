5b3275bd89ee8d8d7a43a41337dee565d02c9912
AsaMitaka <cepelits@gmail.com>
2023-12-23 21:56:58 +0200

    + core const

---
+++ src/core/colors.ts
@@ -0,0 +1,37 @@
+export const COLORS = {
+  darkMode: {
+    backgroundColor: '#222',
+    paddingHorizontal: 20,
+    paddingVertical: 20,
+  },
+  lightMode: {
+    backgroundColor: '#fff',
+
+    paddingHorizontal: 20,
+    paddingVertical: 20,
+  },
+};
+
+export const TEXT = {
+  darkMode: {
+    color: '#ffffff',
+    fontSize: 16,
+    fontWeight: '600',
+  },
+  lightMode: {
+    color: '#10100F',
+    fontSize: 16,
+    fontWeight: '600',
+  },
+};
+
+export const BTN = {
+  alignItems: 'center',
+  background: 'green',
+  borderRadius: 5,
+  color: 'white',
+  fontSize: 16,
+  fontWeight: '600',
+  paddingHorizontal: 10,
+  paddingVertical: 7,
+};

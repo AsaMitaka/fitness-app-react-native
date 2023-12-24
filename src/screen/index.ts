44c28a3b8d686dbe4f3f835387a4f1bc501e866e
AsaMitaka <cepelits@gmail.com>
2023-12-24 23:36:33 +0200

    * new exports

--- src/screen/index.ts
+++ src/screen/index.ts
@@ -1,5 +1,6 @@
 export { default as MenuScreen } from './MenuScreen';
 export { default as NoInternetScreen } from './NoInternetScreen';
+export { default as ProfileScreen } from './ProfileScreen';
 export { default as SettingScreen } from './SettingScreen';
 export { default as StartScreen } from './StartScreen';
 export { default as StatScreen } from './StatScreen';

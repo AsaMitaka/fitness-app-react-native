e48577ba62f4d2a138131d4f9ce2c0363a0a6b14
AsaMitaka <cepelits@gmail.com>
2023-12-24 23:35:11 +0200

    * new screens

--- src/navigation/types.ts
+++ src/navigation/types.ts
@@ -3,22 +3,28 @@ import { NativeStackScreenProps } from '@react-navigation/native-stack';
 type StackParamList = {
   Menu: undefined;
   NoInternet: undefined;
-  Workout: undefined;
+  HomeScreen: undefined;
+  Profile: undefined;
   Setting: undefined;
   Start: undefined;
   Stat: undefined;
+  Workout: undefined;
 };
 
+type HomeScreenProps = NativeStackScreenProps<StackParamList, 'HomeScreen'>;
 type MenuScreenProps = NativeStackScreenProps<StackParamList, 'Menu'>;
 type NoInternetScreenProps = NativeStackScreenProps<StackParamList, 'NoInternet'>;
+type ProfileScreenProps = NativeStackScreenProps<StackParamList, 'Profile'>;
 type SettingScreenProps = NativeStackScreenProps<StackParamList, 'Setting'>;
 type StartScreenProps = NativeStackScreenProps<StackParamList, 'Start'>;
 type StatScreenProps = NativeStackScreenProps<StackParamList, 'Stat'>;
 type WorkoutScreenProps = NativeStackScreenProps<StackParamList, 'Workout'>;
 
 export {
+  HomeScreenProps,
   MenuScreenProps,
   NoInternetScreenProps,
+  ProfileScreenProps,
   SettingScreenProps,
   StackParamList,
   StartScreenProps,
